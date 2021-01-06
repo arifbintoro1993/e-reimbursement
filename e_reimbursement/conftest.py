d33707b007e85eb913f66c45246083f67852334c
Arif Bintoro <arif.bintoro@tech.indosterling.com>
2021-01-06 21:46:58 +0700

    initial commit

---
+++ e_reimbursement/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from e_reimbursement.users.models import User
+from e_reimbursement.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()

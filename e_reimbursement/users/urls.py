6d2e9f36da52fd9951c2b47b1969e12612341a1b
Arif Bintoro <arif.bintoro@tech.indosterling.com>
2021-01-10 03:40:36 +0700

    skeleton template

--- e_reimbursement/users/urls.py
+++ e_reimbursement/users/urls.py
@@ -4,9 +4,11 @@ from e_reimbursement.users.views import (
     user_detail_view,
     user_redirect_view,
 )
+from e_reimbursement.users.views import UserListView
 
 app_name = "users"
 urlpatterns = [
+    path("/", UserListView.as_view(), name="list"),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("<str:username>/", view=user_detail_view, name="detail"),
 ]

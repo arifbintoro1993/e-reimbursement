f29441fe4c4ed4266322908d2fe4d44dd99a9c70
Arif Bintoro <arif.bintoro@tech.indosterling.com>
2021-01-12 03:01:07 +0700

    add pie chart

--- e_reimbursement/reimbursement/views.py
+++ e_reimbursement/reimbursement/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import TemplateView
+from django.db.models import Sum, Value
+from django.db.models.functions import Coalesce
+
 
 from e_reimbursement.reimbursement.api.serializers import ReimbursementSerializers
 from e_reimbursement.otp.forms import OTPVerificationForm
+from e_reimbursement.reimbursement.models import Reimbursement
 
 
 class ReimbursementListView(TemplateView):
@@ -11,4 +15,15 @@ class ReimbursementListView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["serializer"] = ReimbursementSerializers
         context["otp_verification_form"] = OTPVerificationForm
+        context["transport"] = Reimbursement.objects.filter(
+            category=Reimbursement.CATEGORY_CHOICES.transport
+        ).aggregate(total=Coalesce(Sum("amount"), Value(0)))['total']
+        context["fnb"] = Reimbursement.objects.filter(
+            category=Reimbursement.CATEGORY_CHOICES.food_and_beverages
+        ).aggregate(total=Coalesce(Sum("amount"), Value(0)))['total']
+        context["office_supplies"] = Reimbursement.objects.filter(
+            category=Reimbursement.CATEGORY_CHOICES.office_supplies
+        ).aggregate(total=Coalesce(Sum("amount"), Value(0)))['total']
+
+        print(context)
         return context
